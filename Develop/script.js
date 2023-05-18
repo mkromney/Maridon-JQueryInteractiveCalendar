ec1e2985fdbff3295efd96256826f1a721503cd6
Maridon Romney <mkromney@gmail.com>
2023-05-17 22:41:39 -0600

    I have worked out most of the technical issues in the code. There is still some debugging to do, checking the currentTime, otherwise I also need to clean up the commented lines, write up the readme, add a screenshot and double check the feedback from previous HWs to make sure I am not missing anything.

--- Develop/script.js
+++ Develop/script.js
@@ -12,31 +12,55 @@ $(document).ready(function() {
   var currentHour = new Date().getUTCHours(); //24-hour format. //
 
   $('div[id^="hour-"]').each(function() {
-    var divHour = parseInt($(this).attr('id').split('-')[1]);
+    var id = $(this).attr('id');
+    var divHour = parseInt((id).split('-')[1]);
+    console.log(divHour);
+    console.log(id);
+    var text = localStorage.getItem(id);
+    var childEl = $(this).children()[1];
+    $(childEl).text(text)
+    // currentHour = dayjs().hour()
+    currentHour = 23 //can use for testing but make sure to comment out or remove
+    console.log(currentHour,divHour)
+
     //for each div id that begins with hour- this .each function iterates over all items that begin with hour and using the if logic below the number that follows hour- is used to compare the id hour with the actual current hour. Help found in multiple places: https://api.jquery.com/attribute-starts-with-selector/ for the '[name^="value"] bit, for the complicated 'split' where I needed to isolate the individual numbers after 'hour-' I found help here: https://teamtreehouse.com/community/split-string-with-jquery. //
 
     if (divHour < currentHour) {
       $(this).addClass('past');
-    } else if (divHour === currentHour) {
+    } else if (divHour == currentHour) {
       $(this).addClass('present');
+      $(this).removeClass('past');
     } else {
       $(this).addClass('future');
+      $(this).removeClass('present');
+      $(this).removeClass('past');
     }
   });
  
 
 
 // This section is concerned with the click functionality and data storage. //
-$(function () {
+
+   // Click event listener for the save buttons
+  $('.saveBtn').on('click', function(event) {
+    var targetParentId = $(event.target).parent().attr('id');
+    console.log(targetParentId)
+    // var timeBlockId = $(this).closest('.time-block').attr('id');
+    var userInput = $(this).siblings('textarea').val();
+    console.log(userInput)
+    // Saves user input in local storage using the time block ID as the key
+    localStorage.setItem(targetParentId, userInput);
     
-  onclick
+  });
+
+  
 
   // TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage. HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? How might the id be useful when saving the description in local storage?
 
-  // TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour. HINTS: How can the idattribute of each time-block be used to conditionally add or remove thepast, present, and future classes? How can Day.js be used to get the current hour in 24-hour time?
+  
     
   // TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding text area elements. HINT: How can the id attribute of each time-block be used to do this?
   
   // TODO: Add code to display the current date in the header of the page.
   });
-});
+// });
